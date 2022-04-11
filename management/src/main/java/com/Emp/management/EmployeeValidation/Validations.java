a8613dce80f99f63fabd5863991352f6f616a00e
Dinesh635 <dinesh.kumar1@mobikwik.com>
2022-04-11 10:34:51 +0530

    validation added

--- management/src/main/java/com/Emp/management/EmployeeValidation/Validations.java
+++
@@ -1,9 +0,0 @@
-package com.Emp.management.EmployeeValidation;
-
-public interface Validations {
-
-    public boolean check_email(String email);
-    public boolean check_name(String name);
-    public boolean check_mobNo(String mob_no);
-    public boolean check_id(int emp_id);
-}
